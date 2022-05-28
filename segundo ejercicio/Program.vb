66a56794d61706dcf5ae3a76c9bd9702a7c01deb
Ezequiel <insa940@gmail.com>
2022-05-27 22:34:04 -0300

    subiendo segundo ejercicio

---
+++ segundo ejercicio/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("ingrese su nombre: ")
+        Dim nom As String = Console.ReadLine()
+
+        Console.WriteLine("hola " + nom + " como estas?")
+
+    End Sub
+End Module
